a41054019515846fea34e5d3f70f2fc7adf7ce1f
Mano Lemmens <lemmensmano@gmail.com>
2023-09-08 16:48:47 +0200

    solution 0118 in rust

---
+++ rust/src/0118-pascals_triangle.rs
@@ -0,0 +1,25 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let mut vec: Vec<Vec<i32>> = vec![];
+        for i in 0..num_rows {
+            let prevvec = vec.get((i - 1) as usize);
+            let mut newvec: Vec<i32> = vec![];
+                for j in 0..i {
+                let a = prevvec
+                    .unwrap()
+                    .get((j-1) as usize)
+                    .unwrap_or(&0);
+                let b = prevvec
+                    .unwrap()
+                    .get((j) as usize)
+                    .unwrap_or(&0);
+                newvec.push(a + b);
+            }
+            newvec.push(1);
+            vec.push(newvec);
+        }
+        return vec;
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,6 @@
+#[path ="./0118-pascals_triangle.rs"] mod pascals_triangle;
 #[path ="./0189-rotate_array.rs"] mod rotate_array;
 
 fn main() {
-    let mut nums = vec![1,2,3,4,5,6,7];
-    let k = 3;
-    println!("{:?}", nums);
-    rotate_array::Solution::rotate(&mut nums, k);
-    println!("{:?}", nums);
+    println!("{:?}", pascals_triangle::Solution::generate(5))
 }
