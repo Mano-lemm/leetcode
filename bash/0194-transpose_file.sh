cfd303140aae3325d7c7121fc25ee1f8c629bc37
Mano Lemmens <lemmensmano@gmail.com>
2023-09-09 11:54:02 +0200

    solution bash problem 0194

---
+++ bash/0194-transpose_file.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+gawk '{
+       for (f = 1; f <= NF; f++) { a[NR, f] = $f } 
+     }
+     NF > nf { nf = NF }
+     END {
+       for (f = 1; f <= nf; f++) {
+           for (r = 1; r <= NR; r++) {
+               printf a[r, f] (r==NR ? RS : FS)
+           }
+       }
+    }' ./0194-transpose_file.txt
