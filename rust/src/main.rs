21dd8351dd5797704e324263cacf98a5a5fee4ae
Mano Lemmens <lemmensmano@gmail.com>
2023-09-14 11:28:48 +0200

    start merge k sorted list, rust was a bad choice for this

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,7 @@
 #[path ="./0118-pascals_triangle.rs"] mod pascals_triangle;
 #[path ="./0189-rotate_array.rs"] mod rotate_array;
 #[path ="./0015-three_sum.rs"] mod three_sum;
+#[path ="./0023-merge_k_sorted_lists.rs"] mod merge_k_sorted;
 
 fn main() {
 }
--- unfinished.md
+++ unfinished.md
@@ -1,3 +1,3 @@
 # UNFINISHED PROBLEMS
 
-> all done
+> [23: merge k sorted lists (rust)](./rust/src/0023-merge_k_sorted_lists.rs)
