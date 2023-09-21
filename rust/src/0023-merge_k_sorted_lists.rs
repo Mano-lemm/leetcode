7477fdc33546d67fb16465b494786d886c6bfac7
Mano Lemmens <lemmensmano@gmail.com>
2023-09-21 16:15:21 +0200

    solve 23 in rust

--- rust/src/0023-merge_k_sorted_lists.rs
+++ rust/src/0023-merge_k_sorted_lists.rs
@@ -1,22 +1,46 @@
+pub use std::collections::BinaryHeap;
+
 pub struct Solution;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
 }
+
+impl PartialOrd for ListNode {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for ListNode {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.val.cmp(&other.val).reverse()
+    }
+}
+
 impl Solution {
     pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
-        return None;
+        let mut heap = lists
+            .into_iter()
+            .filter_map(|x| x)
+            .collect::<BinaryHeap<_>>();
+        let mut head = None;
+        let mut tail = &mut head;
+        while let Some(node) = heap.pop() {
+            tail = &mut tail.insert(node).next;
+            if let Some(next) = tail.take() {
+                heap.push(next)
+            }
+        }
+        return head;
     }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,37 @@
-#[path ="./0118-pascals_triangle.rs"] mod pascals_triangle;
-#[path ="./0189-rotate_array.rs"] mod rotate_array;
-#[path ="./0015-three_sum.rs"] mod three_sum;
-#[path ="./0023-merge_k_sorted_lists.rs"] mod merge_k_sorted;
+use merge_k_sorted::ListNode;
+
+#[path = "./0023-merge_k_sorted_lists.rs"]
+mod merge_k_sorted;
+#[path = "./0118-pascals_triangle.rs"]
+mod pascals_triangle;
+#[path = "./0189-rotate_array.rs"]
+mod rotate_array;
+#[path = "./0015-three_sum.rs"]
+mod three_sum;
 
 fn main() {
+    let l1: Vec<Option<Box<ListNode>>> = vec![
+        Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 4,
+                next: Some(Box::new(ListNode { val: 5, next: None })),
+            })),
+        })),
+        Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 3,
+                next: Some(Box::new(ListNode { val: 4, next: None })),
+            })),
+        })),
+        Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode { val: 6, next: None })),
+        })),
+    ];
+    let l3: Vec<Option<Box<ListNode>>> = vec![None];
+    println!("{:?}", merge_k_sorted::Solution::merge_k_lists(l1));
+    println!("{:?}", merge_k_sorted::Solution::merge_k_lists(vec![]));
+    println!("{:?}", merge_k_sorted::Solution::merge_k_lists(l3));
 }
