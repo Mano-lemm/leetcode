afb58b0da145bb56aea146671bee7e9e14b8cd0a
Mano Lemmens <lemmensmano@gmail.com>
2023-09-23 14:48:44 +0200

    solve 150 in go

---
+++ go/0150-evaulate_reverse_polish_notation.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"strconv"
+)
+
+type stack []int
+
+func (s *stack) push(n int) {
+	*s = append(*s, n)
+}
+
+func (s *stack) pop() int {
+	r := (*s)[len(*s) - 1]
+	*s = (*s)[0 : len(*s)-1]
+	return r
+}
+
+func evalRPN(tokens []string) int {
+	st := make(stack, 0)
+	for _, value := range tokens {
+		switch value {
+		case "+":
+			left := st.pop()
+			right := st.pop()
+			st.push(right + left)
+		case "-":
+			left := st.pop()
+			right := st.pop()
+			st.push(right - left)
+		case "/":
+			left := st.pop()
+			right := st.pop()
+			st.push(right / left)
+		case "*":
+			left := st.pop()
+			right := st.pop()
+			st.push(right * left)
+		default:
+			v, _ := strconv.ParseInt(value, 10, 16)
+			st.push(int(v))
+		}
+	}
+	return st.pop()
+}
