9aed92a674f2fb671d5f21bc487c5f621b2f00f8
Mano Lemmens <lemmensmano@gmail.com>
2023-09-28 13:54:31 +0200

    solve 905 in go

---
+++ go/0905-sort_array_by_parity.go
@@ -0,0 +1,13 @@
+package main
+
+func sortArrayByParity(nums []int) []int {
+	cidx := -1
+	for i := 0; i < len(nums) - 1; i++ {
+		if nums[i] % 2 == 0{
+			cidx++
+			nums[cidx], nums[i] = nums[i], nums[cidx]
+		}
+	}
+	nums[cidx + 1], nums[len(nums) - 1] = nums[len(nums) - 1], nums[cidx + 1]
+	return nums
+}
