b865f9daa3e7d529bd51fbb922504fbcac4d1bdd
Mano-lemm <Lemmensmano@gmail.com>
2023-10-12 12:05:28 +0200

    solve 1095 in c#
    I failed to first try this because of off by one errors feelsbadman

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,10 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using csharp;
 
-var mininum = new _2009_minimum_number_of_operations_to_make_array_continuous();
+var mntn = new _1095_find_in_mountain_array();
 
-// Console.WriteLine(mininum.MinOperations(new int[] { 4, 2, 5, 3 }));
-// Console.WriteLine(mininum.MinOperations(new int[] { 1, 2, 3, 5, 6 }));
-// Console.WriteLine(mininum.MinOperations(new int[] { 1, 2, 2, 400, 2 }));
-// Console.WriteLine(mininum.MinOperations(new int[] { 1, 10, 100, 1000 }));
-Console.WriteLine(mininum.MinOperations(new int[] { 1, 2, 3, 10, 20 }));
+mntn.FindInMountainArray(0, new MountainArray(new List<int>() { 1, 2, 3, 5, 3 }));
