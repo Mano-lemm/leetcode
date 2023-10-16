4f150e65abfc7ae081e5351d8d36bbb2002c1592
Mano-lemm <Lemmensmano@gmail.com>
2023-10-16 09:51:54 +0200

    solve 119 in c#

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using csharp;
-
-var mntn = new _1095_find_in_mountain_array();
-
-mntn.FindInMountainArray(0, new MountainArray(new List<int>() { 1, 2, 3, 5, 3 }));
