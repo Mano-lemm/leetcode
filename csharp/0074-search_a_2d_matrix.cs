b8ca274d846f3fc88ff118d841015b3cb0e2a4ac
Mano-lemm <Lemmensmano@gmail.com>
2023-10-23 12:41:27 +0200

    solve 74 in c#

---
+++ csharp/0074-search_a_2d_matrix.cs
@@ -0,0 +1,33 @@
+﻿namespace csharp;
+
+public class _0074_search_a_2d_matrix {
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        if (matrix == null || matrix[0] == null)
+        {
+            return false;
+        }
+
+        var mat = matrix.SelectMany(x => x).ToArray();
+        var low = 0;
+        var high = mat.Length;
+        while (low < high)
+        {
+            var mid = (high + low) / 2;
+            if (mat[mid] == target)
+            {
+                return true;
+            } 
+            if (mat[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+
+        return false;
+    }    
+}
