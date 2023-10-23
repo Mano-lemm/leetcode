ff055335b1dfea01f70c8b188fffd95733c5000e
Mano-lemm <Lemmensmano@gmail.com>
2023-10-23 13:54:02 +0200

    improved solution of 74 in c#

--- csharp/0074-search_a_2d_matrix.cs
+++ csharp/0074-search_a_2d_matrix.cs
@@ -1,24 +1,21 @@
-﻿namespace csharp;
+﻿using System.Diagnostics.Contracts;
+
+namespace csharp;
 
 public class _0074_search_a_2d_matrix {
     public bool SearchMatrix(int[][] matrix, int target)
     {
-        if (matrix == null || matrix[0] == null)
-        {
-            return false;
-        }
-
-        var mat = matrix.SelectMany(x => x).ToArray();
         var low = 0;
-        var high = mat.Length;
+        var high = matrix.Length * matrix[0].Length;
         while (low < high)
         {
             var mid = (high + low) / 2;
-            if (mat[mid] == target)
+            var (row, col) = Math.DivRem(mid, matrix[0].Length);
+            if (matrix[row][col] == target)
             {
                 return true;
             } 
-            if (mat[mid] < target)
+            if (matrix[row][col] < target)
             {
                 low = mid + 1;
             }
