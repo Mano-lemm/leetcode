207694a82a4bd705aedc27fa32751fae0f0ac690
Mano-lemm <Lemmensmano@gmail.com>
2023-10-24 13:20:52 +0200

    solve 515 and 1653 in c#

---
+++ csharp/0515-find_largest_value_in_each_tree_row.cs
@@ -0,0 +1,26 @@
+﻿namespace csharp;
+
+public class TreeNode {
+     public int val;
+     public TreeNode? left;
+     public TreeNode? right;
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+}
+
+public class _0515_find_largest_value_in_each_tree_row {
+    public IList<int> LargestValues(TreeNode root)
+    {
+        var r = new List<int>();
+        var c = new List<TreeNode>() { root }.Where(x => x != null).ToList();
+        while (c.Count > 0)
+        {
+            r.Add(c.Select(x => x.val).Max());
+            c = c.SelectMany(x => new[] {x.left, x.right}).Where(x => x != null).ToList();
+        }
+        return r;
+    } 
+}
---
+++ csharp/1653-minimum_deletions_to_make_string_balanced.cs
@@ -0,0 +1,87 @@
+﻿namespace csharp;
+
+public static class LinqExtension
+{
+    public static IEnumerable<TAccumulate> Scan<TSource, TAccumulate>(this IEnumerable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> accumulator)
+    {
+        var acc = new List<TAccumulate>() { seed };
+        var curr = seed;
+        foreach (var item in source)
+        {
+            curr = accumulator(curr, item);
+            acc.Add(curr);
+        }
+        return acc;
+    }
+    
+    public static IEnumerable<TAccumulate> ScanBack<TSource, TAccumulate>(this IEnumerable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> accumulator)
+    {
+        var acc = new List<TAccumulate>() { seed };
+        var curr = seed;
+        foreach (var item in source.Reverse())
+        {
+            curr = accumulator(curr, item);
+            acc.Insert(0, curr);
+        }
+        return acc;
+    }
+}
+
+public class _1653_minimum_deletions_to_make_string_balanced {
+    // dynamic programming solution
+    public int MinimumDeletions(string s)
+    {
+        int cntb = 0, res = 0;
+        foreach (var x in s) {
+            if (x == 'a') res = Math.Min(++res, cntb);
+            else if (x == 'b') ++cntb;
+        }
+        return res;
+    }
+    
+    public int MinimumDeletionsFunctional(string s)
+    {
+        var axs = s
+            .Select(x => x == 'a' ? 1 : 0)
+            .ScanBack(0, (x, y) => x + y);
+        var bxs = s
+            .Select(x => x == 'a' ? 1 : 0)
+            .Scan(0, (x, y) => x + y);
+        return axs
+            .Zip(bxs)
+            .Select(x => x.First + x.Second).Min();
+    }
+    
+    public int MinimumDeletionsImperative(string s)
+    {
+        var acr = 0;
+        var axs = new List<int>() {acr};
+        var bcr = 0;
+        var bxs = new List<int>() {bcr};
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == 'b')
+            {
+                bcr++;
+            }
+
+            if (s[^(i + 1)] == 'a')
+            {
+                acr++;
+            }
+            axs.Insert(0, acr);
+            bxs.Add(bcr);
+        }
+
+        var min = int.MaxValue;
+        foreach (var VARIABLE in axs.Zip(bxs))
+        {
+            if (VARIABLE.First + VARIABLE.Second < min)
+            {
+                min = VARIABLE.First + VARIABLE.Second;
+            }
+        }
+
+        return min;
+    } 
+}
