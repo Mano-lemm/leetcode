73a6183c7bb7c6bf9583ee8492184e259e30f9d8
Mano-lemm <Lemmensmano@gmail.com>
2023-10-26 16:29:22 +0200

    some code beautification

--- csharp/0823-binary_trees_with_factors.cs
+++ csharp/0823-binary_trees_with_factors.cs
@@ -15,10 +15,9 @@ public class _0823_binary_trees_with_factors
         foreach (var i in arr) {
             foreach (var j in arr) {
                 if (j > Math.Sqrt(i)) break;
-                if (i % j == 0 && numbers.Contains(i / j)) {
-                    var temp = combinations[j] * combinations[i / j];
-                    combinations[i] += (i / j == j ? temp : temp * 2) % maxModulo;
-                }
+                if (i % j != 0 || !numbers.Contains(i / j)) continue;
+                var temp = combinations[j] * combinations[i / j];
+                combinations[i] += (i / j == j ? temp : temp * 2) % maxModulo;
             }
         }
         
