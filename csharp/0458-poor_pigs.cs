b0ac97980332160322fb3ef3565fc40705c20aae
Mano Lemmens <lemmensmano@gmail.com>
2023-10-29 12:54:50 +0100

    solve 458 in c#

---
+++ csharp/0458-poor_pigs.cs
@@ -0,0 +1,13 @@
+﻿namespace csharp;
+
+public class _0458_poor_pigs
+{
+    // this was just a math problem
+    // no programming skill involved unfortunately
+    public int PoorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        var c = 0;
+        while(Math.Pow(minutesToTest / minutesToDie + 1, c) < buckets)
+            c++;
+        return c;
+    }
+}
