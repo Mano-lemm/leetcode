08f52d70ab86b06050edbe90a54fdcbc7057d07f
Mano-lemm <Lemmensmano@gmail.com>
2023-10-30 14:00:04 +0100

    solve 1356 in c#

---
+++ csharp/1356-sort_integers_by_the_number_of_1_bits.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+
+namespace csharp;
+
+public class _1356_sort_integers_by_the_number_of_1_bits
+{
+    public int[] SortByBits(int[] arr)
+    {
+        return arr.OrderBy(i => BitOperations.PopCount((uint)i)).ThenBy(x => x).ToArray();
+    }
+    
+    public int[] SortByBitsInPlace(int[] arr)
+    {
+        Array.Sort(arr, (i, i1) => 
+            Popcnt.PopCount((uint)i) - Popcnt.PopCount((uint)i1) != 0 
+                ? (int)(Popcnt.PopCount((uint)i) - Popcnt.PopCount((uint)i1)) 
+                : i - i1);
+        return arr;
+    }
+}
