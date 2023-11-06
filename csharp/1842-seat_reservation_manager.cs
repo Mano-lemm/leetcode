d0a24541810d29a7abc4ffdcda3cb5d93d466d7b
Mano-lemm <Lemmensmano@gmail.com>
2023-11-06 10:46:28 +0100

    dissapointingly easy, the implementation in a functional way is probably a monad

---
+++ csharp/1842-seat_reservation_manager.cs
@@ -0,0 +1,22 @@
+﻿namespace csharp;
+
+public class _1842_seat_reservation_manager
+{
+    private int _top = 0;
+    private readonly PriorityQueue<int, int> _free = new();
+    public _1842_seat_reservation_manager(int n) { }
+    
+    public int Reserve()
+    {
+        return _free.Count == 0 ? _top++ : _free.Dequeue();
+    }
+    
+    public void Unreserve(int seatNumber) {
+        if (seatNumber == _top - 1)
+        {
+            _top--;
+            return;
+        }
+        _free.Enqueue(seatNumber, seatNumber);
+    }
+}
