1bfc81178ecc4368acb63a4880f1ab9c53f926e1
Mano-lemm <Lemmensmano@gmail.com>
2023-11-07 11:04:38 +0100

    solve 1921 in c# (LINQ + SLOW)
    the actual working code

---
+++ csharp/1921-eliminate_maximum_number_of_monsters.cs
@@ -0,0 +1,21 @@
+﻿namespace csharp;
+
+public class _1921_eliminate_maximum_number_of_monsters
+{
+    public static IEnumerable<int> nat(){
+        var i = 0;
+        while(true)
+            yield return i++;
+    }
+
+    public int EliminateMaximum(int[] dist, int[] speed) {
+        var r = dist
+            .Zip(speed)
+            .Select(x => x.First / (double)x.Second)
+            .OrderBy(x => x)
+            .Zip(nat())
+            .FirstOrDefault(x => x.First <= x.Second);
+        var rr = r.Second;
+        return rr == 0 ? dist.Length : rr;
+    }
+}
