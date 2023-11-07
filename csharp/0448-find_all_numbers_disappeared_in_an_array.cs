c1dde65674aebbc1758805a5878561036e54a211
Mano-lemm <Lemmensmano@gmail.com>
2023-11-07 17:25:30 +0100

    solve 448 in c#
    very consise solution, not very fast but very pleasing

---
+++ csharp/0448-find_all_numbers_disappeared_in_an_array.cs
@@ -0,0 +1,17 @@
+﻿namespace csharp;
+
+public class _0448_find_all_numbers_disappeared_in_an_array
+{
+    public static IEnumerable<int> Nat(){
+        var i = 1;
+        while(true)
+            yield return i++;
+    }
+
+    public IList<int> FindDisappearedNumbers(int[] nums) {
+        return Nat()
+            .Take(nums.Length)
+            .Except(nums)
+            .ToList();
+    }
+}
