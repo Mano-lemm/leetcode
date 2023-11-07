b8a18500190b086dc2284058d45a2b25001d2eeb
Mano-lemm <Lemmensmano@gmail.com>
2023-11-07 11:24:34 +0100

    faster solution for 1921

--- csharp/1921-eliminate_maximum_number_of_monsters.cs
+++ csharp/1921-eliminate_maximum_number_of_monsters.cs
@@ -18,4 +18,23 @@ public class _1921_eliminate_maximum_number_of_monsters
         var rr = r.Second;
         return rr == 0 ? dist.Length : rr;
     }
+    
+    public int EliminateMaximumCountingSort(int[] dist, int[] speed){
+        var n = dist.Length;
+        for (var i = 0; i < n; i++) {
+            dist[i] = (int) Math.Ceiling(dist[i] /(double) speed[i]);
+            speed[i] = 0;
+        }
+        foreach(var num in dist) {
+            if (num >= n) continue;
+            speed[num]++;
+        }
+        for (var i = 1; i < n; i++) {
+            speed[i] += speed[i - 1];
+            if (speed[i] > i) {
+                return i;
+            }
+        }
+        return n;
+    }
 }
