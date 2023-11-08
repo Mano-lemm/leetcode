700240d247ed6065b8ca9fe9e93dcae06f6a9d9a
Mano-lemm <Lemmensmano@gmail.com>
2023-11-08 10:00:10 +0100

    solve 2849 in c#

---
+++ csharp/2849-determine_if_a_cel_is_reachable_at_a_given_time.cs
@@ -0,0 +1,13 @@
+﻿namespace csharp;
+
+public class _2849_determine_if_a_cel_is_reachable_at_a_given_time
+{
+    public bool IsReachableAtTime(int sx, int sy, int fx, int fy, int t) 
+    {
+	var minDist = Math.Min(Math.Abs(fx - sx), Math.Abs(fy - sy)) + Math.Abs(Math.Abs(fy - sy) - Math.Abs(fx - sx));
+        if (minDist == 0) {
+            return t == 1 ? false : true;
+        }
+        return t >= minDist;
+    }
+}
