2aa6aefeb9f0ec6f39dc988cd8acfa8653493660
Mano-lemm <Lemmensmano@gmail.com>
2023-11-08 16:32:37 +0100

    formatting

--- csharp/0448-find_all_numbers_disappeared_in_an_array.cs
+++ csharp/0448-find_all_numbers_disappeared_in_an_array.cs
@@ -2,16 +2,26 @@
 
 public class _0448_find_all_numbers_disappeared_in_an_array
 {
-    public static IEnumerable<int> Nat(){
+    public static IEnumerable<int> Nat()
+    {
         var i = 1;
-        while(true)
+        while (true)
             yield return i++;
     }
 
-    public IList<int> FindDisappearedNumbers(int[] nums) {
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
         return Nat()
             .Take(nums.Length)
             .Except(nums)
+            .ToList();
+    }
+
+    public IList<int> FindDisappearedNumbers2(int[] nums)
+    {
+        return Nat()
+            .Except(nums)
+            .TakeWhile(x => x < nums.Length)
             .ToList();
     }
 }
