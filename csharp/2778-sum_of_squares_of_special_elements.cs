97ba04dbccc1987b3e3e2ac322139fad14207892
Mano-lemm <Lemmensmano@gmail.com>
2023-11-08 14:49:13 +0100

    solve 2778 in c#

---
+++ csharp/2778-sum_of_squares_of_special_elements.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp;
+
+public class _2778_sum_of_squares_of_special_elements
+{
+    public int SumOfSquares(int[] nums) {
+        return nums
+            .Where((_, idx) => nums.Length % (idx + 1) == 0)
+            .Aggregate(0, (a, b) => a + b * b);
+    }
+}
