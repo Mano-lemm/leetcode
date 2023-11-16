a334b09155e3804eba783a2ffd990134dba764a6
Mano-lemm <Lemmensmano@gmail.com>
2023-11-16 15:22:23 +0100

    benchmark 1980
    surprising results according to lc

---
+++ csharp/BenchMarks/1980-benchmark.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace csharp.BenchMarks;
+
+public class _1980_benchmark
+{
+    [Params(10, 100, 1000, 2000, 3000, 4000)]
+    public int N { get; set; }
+    private string[] data { get; set; }
+    private static _1980_find_unique_binary_string solution { get; } = new();
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var rand = new Random();
+        var sb = new StringBuilder();
+        var genRandoms = (int n) => Enumerable
+            .Range(0, n)
+            .Select(_ =>
+            {
+                for (var i = 0; i < n; i++)
+                {
+                    sb.Append('0' + rand.Next(2));
+                }
+
+                var r = sb.ToString();
+                sb.Clear();
+                return r;
+            }).ToArray();
+        data = genRandoms(N); // executed once per each N value
+    }
+
+    [Benchmark]
+    public string ForLoop() => solution.FindDifferentBinaryString(data);
+
+    [Benchmark]
+    public string LINQQuery() => solution.FindDifferentBinaryStringLINQ(data);
+}
