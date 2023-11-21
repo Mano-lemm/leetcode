36fe45b31880d0847d1c858aaa36aaa2d151e34a
Mano-lemm <Lemmensmano@gmail.com>
2023-11-21 16:48:54 +0100

    solve 275 in c#

---
+++ csharp/0275-H_index_II.cs
@@ -0,0 +1,26 @@
+﻿namespace csharp;
+
+public class _0275_H_index_II
+{
+    public int HIndex(int[] citations)
+    {
+        var low = 0;
+        var high = citations.Length;
+        var hi = 0;
+        while (low <= high)
+        {
+            var mid = low + (high - low) / 2;
+            if (citations[mid] >= citations.Length - mid)
+            {
+                high = mid - 1;
+                hi = citations.Length - mid;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return hi;
+    }
+}
