71f91eedaaedaa0a5ce80644af09aad2fa7df672
Mano-lemm <Lemmensmano@gmail.com>
2023-11-22 09:46:01 +0100

    solve 1424 in c#

---
+++ csharp/1424-diagonal_traverse_II.cs
@@ -0,0 +1,35 @@
+﻿namespace csharp;
+
+public class _1424_diagonal_traverse_II
+{
+    public int[] FindDiagonalOrder(IList<IList<int>> nums)
+    {
+        var groups = new Dictionary<int, List<int>>();
+
+        for (var row = nums.Count - 1; row >= 0; row--)
+        {
+            for (var col = 0; col < nums[row].Count; col++)
+            {
+                var diagonal = row + col;
+                if (!groups.TryGetValue(diagonal, out var value))
+                {
+                    value = new List<int>();
+                    groups[diagonal] = value;
+                }
+
+                value.Add(nums[row][col]);
+            }
+        }
+
+        var ans = new List<int>();
+        var curr = 0;
+
+        while (groups.ContainsKey(curr))
+        {
+            ans.AddRange(groups[curr]);
+            curr++;
+        }
+
+        return ans.ToArray();
+    }
+}
