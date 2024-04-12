749cf084d6f1de79047fcbf266f13a90c0e8758b
Mano Lemmens <lemmensmano@gmail.com>
2024-04-12 22:41:48 +0200

    solve 1114 in c

---
+++ c/1114-print_in_order.c
@@ -0,0 +1,56 @@
+#include <pthread.h>
+
+typedef struct
+{
+    pthread_mutex_t mut;
+    pthread_cond_t cond;
+    int cur;
+} Foo;
+
+Foo *fooCreate()
+{
+    Foo *obj = (Foo *)malloc(sizeof(Foo));
+    pthread_mutex_init(&obj->mut, NULL);
+    pthread_cond_init(&obj->cond, NULL);
+    obj->cur = 0;
+    return obj;
+}
+
+void first(Foo *obj)
+{
+    printFirst();
+    obj->cur = 1;
+    pthread_cond_signal(&obj->cond);
+}
+
+void second(Foo *obj)
+{
+    pthread_mutex_lock(&obj->mut);
+    while (obj->cur != 1)
+    {
+        pthread_cond_wait(&obj->cond, &obj->mut);
+    }
+
+    obj->cur = 2;
+    printSecond();
+    pthread_mutex_unlock(&obj->mut);
+    pthread_cond_broadcast(&obj->cond);
+}
+
+void third(Foo *obj)
+{
+    pthread_mutex_lock(&obj->mut);
+    while (obj->cur != 2)
+    {
+        pthread_cond_wait(&obj->cond, &obj->mut);
+    }
+
+    printThird();
+    pthread_mutex_unlock(&obj->mut);
+}
+
+void fooFree(Foo *obj)
+{
+    pthread_cond_destroy(&obj->cond);
+    pthread_mutex_destroy(&obj->mut);
+}
